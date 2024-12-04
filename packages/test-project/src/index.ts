6cd52f86e8043c482e3e1a4c4fc284f4ba13c891
Ronen Amiel <ronen.amiel@gmail.com>
2024-12-04 19:43:09 +0200

    wip

--- packages/test-project/src/index.ts
+++ packages/test-project/src/index.ts
@@ -1,9 +1,32 @@
-function test({
-  used,
-}: {
-  // Unused index signature '[number]' in destructuring.
-  [i: number]: boolean;
-  [i: string]: boolean;
-}) {
+/* eslint-disable no-useless-computed-key */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+declare const d: 0 | 3;
+
+function test1({
+  // '-2': b,
+  ['0']: { a },
+  // [d]: c,
+}: [
+  //
+  // boolean,
+  { a: number },
+  // ...string[],
+  // string,
+  // boolean,
+  // number,
+]) {
   // ...
 }
+
+// declare const e: string;
+
+// function test2({
+//   // '-2': b,
+//   // 1: a,
+//   [e]: c,
+// }: {
+//   hello: number;
+// }) {
+//   // ...
+// }
