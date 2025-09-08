593b4dd57f8a01bf0e93a71ca7f6dac4d158e5fb
Ronen Amiel <ronen.amiel@gmail.com>
2025-09-08 22:41:51 +0300

    chore: remove unnecessary typings for eslint-react-hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 
 import eslintCommentsPlugin from '@eslint-community/eslint-plugin-eslint-comments/configs';
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
-import { FlatCompat } from '@eslint/eslintrc';
 import eslint from '@eslint/js';
 import tseslintInternalPlugin from '@typescript-eslint/eslint-plugin-internal';
 import vitestPlugin from '@vitest/eslint-plugin';
@@ -21,7 +19,6 @@ import url from 'node:url';
 import tseslint from 'typescript-eslint';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const restrictNamedDeclarations = {
   message:
@@ -48,9 +45,7 @@ export default defineConfig(
       ['vitest']: vitestPlugin,
       // https://github.com/facebook/react/issues/28313
       ['react']: reactPlugin,
-      // @ts-expect-error -- Temporary types incompatibility pending flat config support
-      // https://github.com/facebook/react/pull/30774
-      ['react-hooks']: fixupPluginRules(reactHooksPlugin),
+      ['react-hooks']: reactHooksPlugin,
       ['regexp']: regexpPlugin,
       ['unicorn']: unicornPlugin,
     },
@@ -592,14 +587,10 @@ export default defineConfig(
       // https://github.com/facebook/react/pull/30774
       // @ts-expect-error -- Temporary types incompatibility pending flat config support
       { name: 'react/recommended', ...reactPlugin.configs.flat.recommended },
-      fixupConfigRules([
-        {
-          name: 'react-hooks/recommended',
-          // https://github.com/facebook/react/pull/30774
-          // @ts-expect-error -- Temporary types incompatibility pending flat config support
-          ...compat.config(reactHooksPlugin.configs.recommended)[0],
-        },
-      ]),
+      {
+        name: 'react-hooks/recommended',
+        ...reactHooksPlugin.configs.recommended,
+      },
     ],
     files: ['packages/website/**/*.?(c|m)[tj]s?(x)'],
     name: 'website',
